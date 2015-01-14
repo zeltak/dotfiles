3230d482eb79a3e85c6c0fdad2f6f767fe646c93
zeltak <zeltak@gmail.com>
2015-01-14 13:57:12 +0200

    zuni Wed Jan 14 13:57:12 IST 2015

--- LAPTOP/bin/urs.sh
+++ LAPTOP/bin/urs.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-#mirror music library
-rsync -aRPz --delete -e ssh /home/zeltak/music  root@132.72.152.204:/volume1/rsync/     
-
 #Emacs
-rsync -aRPz --delete -e ssh --exclude='.git/'  /home/zeltak/.emacs.d/  root@132.72.152.204:/volume1/rsync/       #&> /dev/null
-
+rsync -aRPz --delete -e ssh --exclude='.git/'  /home/zeltak/.emacs.d/  root@132.72.152.204:/volume1/ursync/       #&> /dev/null
 #org
-rsync -aRPz --delete -L -e ssh --exclude='.git/' /home/zeltak/org/  root@132.72.152.204:/volume1/rsync/           #&> /dev/null
-
+rsync -aRPz --delete -L -e ssh --exclude='.git/' /home/zeltak/org/  root@132.72.152.204:/volume1/ursync/           #&> /dev/null
 #dotfiles
-rsync -aRPz --delete -L -e ssh --exclude='.git/' /home/zeltak/dotfiles/  root@132.72.152.204:/volume1/rsync/       #&> /dev/null
-
+rsync -aRPz --delete -L -e ssh --exclude='.git/' /home/zeltak/dotfiles/  root@132.72.152.204:/volume1/ursync/       #&> /dev/null
 #ssh
-rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/rsync/          #&> /dev/null
+rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/ursync/          #&> /dev/null
 
-rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.mozilla/ root@132.72.152.204:/volume1/rsync/          #&> /dev/null
-
-#kde
-#rsync -aRPz --delete -e ssh /home/zeltak/.kde4/  root@132.72.152.204:/volume1/rsync/          #&> /dev/null
-
-#images/phtos
-rsync -aRPz --delete -e ssh /home/zeltak/Pictures/  root@132.72.152.204:/volume1/rsync/    / &> /dev/null
-
-#ThemeDB
-rsync -aRPz --delete -e ssh /home/zeltak/ThemeDB/  root@132.72.152.204:/volume1/rsync/    / &> /dev/null
-
+rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/.config/          #&> /dev/null
+rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/.fonts/          #&> /dev/null
+rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/.i3/          #&> /dev/null
+rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/R/          #&> /dev/null
+rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/Sync/          #&> /dev/null
+rsync -aRPz --delete -e ssh --exclude='.git/' /home/zeltak/.ssh/  root@132.72.152.204:/volume1/Uni/          #&> /dev/null
 
 
 
 
 ###final notify
-notify-send -u critical "back to synology for today done"
+#notify-send -u critical "back to synology for today done"
