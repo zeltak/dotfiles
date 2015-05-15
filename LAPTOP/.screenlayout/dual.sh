5608b50dfbf95fc251fcb0e3c5e80b3c70db329d
zeltak <zeltak@gmail.com>
2015-05-15 07:50:06 +0300

    zuni Fri May 15 07:50:06 IDT 2015

---
+++ LAPTOP/.screenlayout/dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP2-1 --mode 1280x960 --pos 0x0 --rotate normal --output DP2-2 --mode 2560x1440 --pos 1280x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
