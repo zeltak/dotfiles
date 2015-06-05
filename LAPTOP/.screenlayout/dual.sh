62ec151918e47122c402567374315110c968a573
zeltak <zeltak@gmail.com>
2015-06-05 12:20:01 +0300

    zuni Fri Jun  5 12:20:01 IDT 2015

--- LAPTOP/.screenlayout/dual.sh
+++ LAPTOP/.screenlayout/dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --mode 2560x1440 --pos 1960x0 --rotate normal --output HDMI2 --mode 1920x1200 --pos 0x112 --rotate normal --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --primary --mode 2560x1440 --pos 1960x0 --rotate normal --output HDMI2 --mode 1920x1200 --pos 0x112 --rotate normal --output HDMI1 --off --output DP2 --off
--- LAPTOP/.screenlayout/office.sh
+++ LAPTOP/.screenlayout/office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI1 --off --output DP2 --off
+xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI1 --off --output DP2 --off
